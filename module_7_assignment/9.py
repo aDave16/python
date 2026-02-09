346cf1748b3eb6d8d6be941c6b27b313c9f6259a
aDave16 <amihdave08@gmail.com>
2026-02-09 11:06:47 +0530

    Add files via upload

---
+++ module_7_assignment/9.py
@@ -0,0 +1,11 @@
+#Write a Python program to import the math module and use functions like sqrt(), ceil(),
+#floor().
+#Write a Python program to generate random numbers using the random module.
+import math
+print(math.sqrt(25))
+print(math.ceil(45.78))
+print(math.floor(56.4533))
+
+import random
+print(random.random())
+print(random.randint(20,45))
